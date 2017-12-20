3936d806d1d2c57a54a492cd106e238ab5889a96
John Teran <johntx.jt@gmail.com>
2017-12-20 07:19:46 -0400

    update condition dates in reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -36,8 +36,8 @@ class ReportController extends Controller
 
     public function debit()
     {
-        $fecha_semana = date('Y-m-d',strtotime('-1 week', strtotime(\Carbon\Carbon::now()) ));
-        $fecha_mes = date('Y-m-d',strtotime('+1 month', strtotime(\Carbon\Carbon::now()) ));
+        $fecha_semana = date('Y-m-d',strtotime('-1 month', strtotime(\Carbon\Carbon::now()) ));
+        $fecha_mes = date('Y-m-d',strtotime('+1 week', strtotime(\Carbon\Carbon::now()) ));
         $payments = \Institute\Payment::
         where('payments.estado','Pendiente')
         ->whereBetween('fecha_pagar', array( $fecha_semana, $fecha_mes))
@@ -48,8 +48,8 @@ class ReportController extends Controller
 
     public function groups()
     {
-        $fecha_semana = date('Y-m-d',strtotime('+1 week', strtotime(\Carbon\Carbon::now()) ));
-        $fecha_mes = date('Y-m-d',strtotime('-1 month', strtotime(\Carbon\Carbon::now()) ));
+        $fecha_semana = date('Y-m-d',strtotime('+1 month', strtotime(\Carbon\Carbon::now()) ));
+        $fecha_mes = date('Y-m-d',strtotime('-1 week', strtotime(\Carbon\Carbon::now()) ));
         $startclasses = \Institute\Startclass::
         whereBetween('fecha_inicio',array( $fecha_mes, $fecha_semana))
         ->get();
--- resources/views/admin/report/debit.blade.php
+++ resources/views/admin/report/debit.blade.php
@@ -2,30 +2,32 @@
 @section('content')
 @include('alerts.succes')
 <div class="table-responsive">
-<table class="table table-hover">
-	<thead>
-		<th>Nombre</th>
-		<th>Carrera</th>
-		<th>Inicio</th>
-		<th>Fecha Pagar</th>
-		<th>Saldo</th>
-		<th>Telefono</th>
-		<th>Opción</th>
-	</thead>
-	@foreach($payments as $payment)
-	@if (strtotime($payment->fecha_pagar) < strtotime(\Carbon\Carbon::now()))
+	<table class="table table-hover">
+		<thead>
+			<th>Nombre</th>
+			<th>Carrera</th>
+			<th>Inicio</th>
+			<th>Fecha Pagar</th>
+			<th>Saldo</th>
+			<th>Telefono</th>
+			<th>Opción</th>
+		</thead>
+		@foreach($payments as $payment)
+		@if (strtotime($payment->fecha_pagar) < strtotime(\Carbon\Carbon::now()))
 		<tbody style="background-color: rgba(255,100,0,0.3)">
-	@else
-	<tbody>
-	@endif
-		<td>{{$payment->inscription->people->nombrecompleto()}}</td>
-		<td>{{$payment->inscription->group->startclass->career->nombre}}</td>
-		<td>{{ \Carbon\Carbon::parse($payment->inscription->group->startclass->fecha_inicio)->format('d/m/y')}}</td>
-		<td>{{ \Carbon\Carbon::parse($payment->fecha_pagar)->format('d/m/y')}}</td>
-		<td>{{$payment->saldo}}</td>
-		<td>{{$payment->inscription->people->telefono}}</td>
-	</tbody>
-	@endforeach
-</table>
-</div>
-@endsection
+			@else
+			<tbody>
+				@endif
+				<td>{{$payment->inscription->people->nombrecompleto()}}</td>
+				<td>{{$payment->inscription->group->startclass->career->nombre}}</td>
+				<td>{{ \Carbon\Carbon::parse($payment->inscription->group->startclass->fecha_inicio)->format('d/m/y')}}</td>
+				<td>
+					<b>{{ \Carbon\Carbon::parse($payment->fecha_pagar)->format('d/m/y')}}</b>
+				</td>
+				<td>{{$payment->saldo}}</td>
+				<td>{{$payment->inscription->people->telefono}}</td>
+			</tbody>
+			@endforeach
+		</table>
+	</div>
+	@endsection
