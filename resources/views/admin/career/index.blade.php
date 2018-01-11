b38ac1bf90411475708875b0925f8c8b4a13f7b9
John Teran <johntx.jt@gmail.com>
2018-01-11 17:51:36 -0400

    sin cambios en el pull, añadiendo espacios

--- resources/views/admin/career/index.blade.php
+++ resources/views/admin/career/index.blade.php
@@ -2,31 +2,31 @@
 @section('content')
 @include('alerts.succes')
 <div class="table-responsive">
-<table class="table table-hover">
-	<thead>
-		<th>Id</th>
-		<th>Nombre</th>
-		<th>Edit</th>
-	</thead>
-	@foreach($careers as $career)
-	<tbody>
-		<td>{{$career->id}}</td>
-		<td>{{$career->nombre}}</td>
-		@foreach(Auth::user()->role->functionalities as $func)
-		@if ($func->code=='ECAR')
-		<td>
-			{!!link_to_route('admin.career.edit', $title = 'Editar', $parameters = $career->id, $attributes = ['class'=>'btn btn-primary'])!!}
-		</td>
-		@endif
-		@if ($func->code=='DCAR')
-		<td>
-			{!!link_to_route('admin.career.show', $title = 'Borrar', $parameters = $career->id, $attributes = ['class'=>'btn btn-danger'])!!}
-		</td>
-		@endif
+	<table class="table table-hover">
+		<thead>
+			<th>Id</th>
+			<th>Nombre</th>
+			<th>Edit</th>
+		</thead>
+		@foreach($careers as $career)
+		<tbody>
+			<td>{{$career->id}}</td>
+			<td>{{$career->nombre}}</td>
+			@foreach(Auth::user()->role->functionalities as $func)
+			@if ($func->code=='ECAR')
+			<td>
+				{!!link_to_route('admin.career.edit', $title = 'Editar', $parameters = $career->id, $attributes = ['class'=>'btn btn-primary'])!!}
+			</td>
+			@endif
+			@if ($func->code=='DCAR')
+			<td>
+				{!!link_to_route('admin.career.show', $title = 'Borrar', $parameters = $career->id, $attributes = ['class'=>'btn btn-danger'])!!}
+			</td>
+			@endif
+			@endforeach
+		</tbody>
 		@endforeach
-	</tbody>
-	@endforeach
-</table>
+	</table>
 </div>
 {!!$careers->render()!!}
 @endsection
